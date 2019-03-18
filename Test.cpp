4251380dd2292ba2d83ffdf957ca83f78eb46d94
Mohammad Nazari <mn68728@gmail.com>
2019-03-18 19:20:49 +0330

    change and update

--- Test.cpp
+++ Test.cpp
@@ -3,9 +3,9 @@
 int main()
 {
   float number1 , number2;
-  std::cout << "Enter number 1:";
+  std::cout << "Enter number one please:";
   std::cin >> number1;
-  std::cout << "Enter number 2:";
+  std::cout << "Enter number two please:";
   std::cin >> number2;
   std::cout << number1 <<"+"<<number2<<"="<<(number1+number2)<<'\n';
   std::cout << number1 <<"-"<<number2<<"="<<(number1-number2)<<'\n';
